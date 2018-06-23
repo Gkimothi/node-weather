92e9c2437427b10a5daede114431b111178bd867
Gaurav Kimothi <gaurav.kimothi@gmail.com>
2018-06-23 15:27:43 -0700

    Fixed Heroku Port issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 let app = express();
 
